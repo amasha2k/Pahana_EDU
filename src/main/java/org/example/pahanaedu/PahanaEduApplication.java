79b85358a7292ff31c8444d10a4e5c8753fa3f5d
amasha2k <amahiru1013@gmail.com>
2025-07-21 20:06:30 +0530

    Initial commit

---
+++ src/main/java/org/example/pahanaedu/PahanaEduApplication.java
@@ -0,0 +1,13 @@
+package org.example.pahanaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahanaEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahanaEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/pahanaedu/PahanaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pahanaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahanaEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
